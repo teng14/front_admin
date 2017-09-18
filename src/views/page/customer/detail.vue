8404832e332adb8a0e26b94e1807ea47c24eb6e8
278697698@qq.com <278697698@qq.com>
2017-09-18 18:22:00 +0800

    更新

--- src/views/page/customer/detail.vue
+++ src/views/page/customer/detail.vue
@@ -7,7 +7,7 @@
             <span style="line-height: 36px;">基础信息</span>
           </div>
           <div class="text item">
-            <p>ID: 9999 &nbsp;&nbsp;&nbsp;&nbsp;终端名：德阳市区源生堂大药房</p>
+            <p>ID: 9999 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;终端名：德阳市区源生堂大药房</p>
             <p>联系人：张金祥 &nbsp;&nbsp;&nbsp;&nbsp;电话：18981038656  QQ：无</p>
             <p>账号状态：激活</p>
             <p>资质份数：3，请联系终端补足资质。</p>
@@ -19,7 +19,8 @@
         <el-card class="box-card">
           <div slot="header" class="clearfix">
             <span style="line-height: 36px;">最近一次客情记录</span>
-            <el-button style="float: right;" type="primary">操作按钮</el-button>
+            <el-button style="float: right;" type="primary">所有记录</el-button>
+            <el-button style="float: right; margin-right: 10px;" type="primary">新增客情记录</el-button>
           </div>
           <div class="text item">
             
